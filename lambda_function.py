63a1ced541596e7ff65fabea02137544aa0efe8a
Adrian Hornsby <adhorn@amazon.com>
2019-07-05 12:30:47 +0300

    little improvements to the readme, sls template, etc..

--- lambda_function.py
+++ lambda_function.py
@@ -4,39 +4,35 @@ from chaos_lib import corrupt_statuscode
 from chaos_lib import SessionWithDelay
 
 
-
-os.environ["FAILURE_INJECTION_PARAM"] = 'chaoslambda.config'
-
-
-def dummy1():
-    session = SessionWithDelay(delay=300)
-    session.get('https://stackoverflow.com/')
-    pass
+# def dummy1():
+#     session = SessionWithDelay(delay=300)
+#     session.get('https://stackoverflow.com/')
+#     pass
 
 
-@corrupt_delay
-def dummy2():
-    pass
+# @corrupt_delay
+# def dummy2():
+#     pass
 
 
-@corrupt_expection
-def lambda_handler_example1(event, context):
-    return {
-        'statusCode': 200,
-        'body': 'Hello from Lambda!'
-    }
+# @corrupt_expection
+# def lambda_handler(event, context):
+#     return {
+#         'statusCode': 200,
+#         'body': 'Hello from Lambda!'
+#     }
 
 
-@corrupt_delay
-def lambda_handler_example2(event, context):
-    return {
-        'statusCode': 200,
-        'body': 'Hello from Lambda!'
-    }
+# @corrupt_delay
+# def lambda_handler(event, context):
+#     return {
+#         'statusCode': 200,
+#         'body': 'Hello from Lambda!'
+#     }
 
 
 @corrupt_statuscode
-def lambda_handler_example3(event, context):
+def lambda_handler(event, context):
     return {
         'statusCode': 200,
         'body': 'Hello from Lambda!'
